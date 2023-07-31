2eb0bb604ed883053fb5b96ce7072ad8b196f536
gutudaniil17 <101668867+gutudaniil17@users.noreply.github.com>
2023-07-31 09:25:14 +0300

    Update DatabaseConnectionStrings.java

--- src/main/java/org/example/constants/DatabaseConnectionStrings.java
+++ src/main/java/org/example/constants/DatabaseConnectionStrings.java
@@ -1,7 +1,7 @@
 package org.example.constants;
 
 public class DatabaseConnectionStrings {
-    public static final String URL = "dbc:mysql://localhost:3306/mydb";
+    public static final String URL = "jdbc:mysql://localhost:3306/mydb";
     public static final String USERNAME = "root";
     public static final String PASSWORD = "root";
 }
